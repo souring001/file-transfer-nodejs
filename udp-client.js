e19cea73c42eea415059083f084ec905d1fa8f28
aso <titechei@gmail.com>
2019-09-03 12:25:48 +0900

    UDP: read, send, write file

--- udp-client.js
+++ udp-client.js
@@ -1,10 +1,16 @@
 const dgram = require('dgram');
+const fs = require('fs');
 const settings = require('./settings');
-const message = Buffer.from('Hello World!');
 const client = dgram.createSocket('udp4');
+
+const file = fs.readFileSync('./files/src/file1');
+// console.log(file);
+
+const message = Buffer.from('Hello World!');
+// console.log(message);
 
 client.bind(settings.CLIENT_PORT, settings.CLIENT_HOST);
 
-client.send(message, settings.SERVER_PORT, settings.SERVER_HOST, (err) => {
+client.send(file, settings.SERVER_PORT, settings.SERVER_HOST, (err) => {
   client.close();
 });
--- udp-server.js
+++ udp-server.js
@@ -1,19 +1,21 @@
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
+const fs = require('fs');
 const settings = require('./settings');
+const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
+  console.log('server error:\n' + err.stack);
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+server.on('message', (file, rinfo) => {
+  fs.writeFileSync('./files/dst/file1', file);
+  console.log('server got file from ' + rinfo.address + ':' + rinfo.port);
 });
 
 server.on('listening', () => {
   const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
+  console.log('server listening '+ address.address + ':' + address.port);
 });
 
 server.bind(settings.SERVER_PORT, settings.SERVER_HOST);
